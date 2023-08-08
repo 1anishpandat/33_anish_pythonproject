90945943f477ccb494eb195d11247b8ee2304760
1anishpandat <114794612+1anishpandat@users.noreply.github.com>
2023-08-08 21:23:32 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int find_factorial(int);
+int main()
+{
+	int num, fact;
+	printf("enter any integer number\n");
+	scanf("%d",&num);
+	fact=find_factorial(num);
+	printf(" factorial of %d is %d", num , fact);
+	return 0;
+		
+
+
+int find_factorial(int n);
+
+
+
+if(
+
+n==0)
+ret
+
+
+
+urn(1);
+return(n*finf_factorial(n-1));
+}
